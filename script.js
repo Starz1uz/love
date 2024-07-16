cfed6873bc60f3163df710b063b2b0e351a3a46c
Bexruz <148357539+Starz1uz@users.noreply.github.com>
2024-07-16 11:03:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function openGallery() {
+    document.getElementById('gallery').classList.remove('hidden');
+    document.getElementById('gallery').classList.add('visible');
+}
+
+function closeGallery() {
+    document.getElementById('gallery').classList.remove('visible');
+    document.getElementById('gallery').classList.add('hidden');
+}
